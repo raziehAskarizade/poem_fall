42c0efc4c98a6aba3371d4262a1ef552304ee96b
askari <mehdi.askarizade@gmail.com>
2021-09-04 11:40:21 +0430

    templates in main page

--- weblog/views.py
+++ weblog/views.py
@@ -4,10 +4,10 @@ from .models import Topic
 
 def post_list(request):
     post = Topic.published.all()
-    return render(request, 'templates/weblog/post_list.html', {'post': post})
+    return render(request, 'weblog/post/post_list.html', {'post': post})
 
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Topic, publish__year=year, publish__month=month, publish__day=day,
                              slug=slug, status='published')
-    return render(request, 'templates/weblog/post_detail.html', {'post': post})
+    return render(request, 'weblog/post/post_detail.html', {'post': post})
