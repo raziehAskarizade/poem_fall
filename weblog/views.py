e76932a817defc0a937f5d522fa077fb52985d7c
askari <mehdi.askarizade@gmail.com>
2021-09-05 19:46:14 +0430

    class-based for post-list

--- weblog/views.py
+++ weblog/views.py
@@ -1,10 +1,21 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Topic
 
 
 def post_list(request):
-    post = Topic.published.all()
-    return render(request, 'weblog/post/post_list.html', {'post': post})
+    posts = Topic.published.all()
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+    try:
+        # page_obj
+        post = paginator.page(page_number)
+    except PageNotAnInteger:
+        post = paginator.page(1)
+    except EmptyPage:
+        post = paginator.page(paginator.num_pages)
+
+    return render(request, 'weblog/post/post_list.html', {'page_number': page_number, 'post': post})
 
 
 def post_detail(request, year, month, day, slug):
