98d6002af1e54d40ea71119d28c48bbdec878193
askari <mehdi.askarizade@gmail.com>
2021-09-06 12:31:10 +0430

    get true position for pagination

--- weblog/urls.py
+++ weblog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'weblog'
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- weblog/views.py
+++ weblog/views.py
@@ -1,21 +1,29 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Topic
 
+class PostListView(ListView):
+    queryset = Topic.published.all()
+    context_object_name = 'post'
+    paginate_by = 3
+    template_name = 'weblog/post/post_list.html'
 
-def post_list(request):
-    posts = Topic.published.all()
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get('page')
-    try:
-        # page_obj
-        post = paginator.page(page_number)
-    except PageNotAnInteger:
-        post = paginator.page(1)
-    except EmptyPage:
-        post = paginator.page(paginator.num_pages)
 
-    return render(request, 'weblog/post/post_list.html', {'page_number': page_number, 'post': post})
+# def post_list(request):
+#     posts = Topic.published.all()
+#     paginator = Paginator(posts, 3)
+#     page_number = request.GET.get('page')
+#     try:
+#         # page_obj
+#         post = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         post = paginator.page(1)
+#     except EmptyPage:
+#         post = paginator.page(paginator.num_pages)
+#
+#     return render(request, 'weblog/post/post_list.html', {'page_number': page_number, 'post': post})
 
 
 def post_detail(request, year, month, day, slug):
